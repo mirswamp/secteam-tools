5c4719a3a7bd21ccd49122471e9fd9e548f397d9
zsshah <zsshah@iu.edu>
2019-01-25 17:04:43 -0500

    Updated script to change result filename
    Updated script 1. to change the result filename: now filename will have target IP/hostname and timestamp. 
    2. Script will prompt to check whether we know the target IP or target hostname.

--- port_scan.sh
+++ port_scan.sh
@@ -12,56 +12,58 @@ fi
 
 
 
-read -p "Hostname of a target: " HOSTNAME
-read -p "IP address of a target: " IP
+read -p "Do you know the hostname of a target? Enter 'yes' or 'no': " KNOWN
+if [ "$KNOWN" = "yes" ];
+then
+    	read -p "Hostname of a target: " HOSTNAME
+        TARGET=$HOSTNAME
+else
+        read -p "IP address of a target: " IP
+        TARGET=$IP
+fi
 
 
 
-PORTSCAN_RESULT=result_PortScan-$(date +"%m-%d-%Y").txt
+PORTSCAN_RESULT=portscan-$TARGET-$(date +"%m-%d-%Y-%T").txt
 > $PORTSCAN_RESULT
 
-echo "---------- Port scan using target IP address ----------" | tee -a $PORTSCAN_RESULT
-nmap -Pn $IP >> $PORTSCAN_RESULT
-echo >> $PORTSCAN_RESULT
-echo >> $PORTSCAN_RESULT
-
-echo "---------- Port scan using target hostname ----------" | tee -a $PORTSCAN_RESULT
-nmap -Pn $HOSTNAME >> $PORTSCAN_RESULT
+echo "---------- Port scan without host discovery ----------" | tee -a $PORTSCAN_RESULT
+nmap -Pn $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
 echo "---------- Scan 100 most common ports (Fast) ----------" | tee -a $PORTSCAN_RESULT
-nmap -Pn -F $IP >> $PORTSCAN_RESULT
+nmap -Pn -F $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
 echo "---------- Scan all 65535 ports ----------" | tee -a $PORTSCAN_RESULT
-nmap -Pn -p- $IP >> $PORTSCAN_RESULT
+nmap -Pn -p- $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
 echo "---------- Detect OS and Services ----------" | tee -a $PORTSCAN_RESULT
-nmap -Pn -A $IP >> $PORTSCAN_RESULT
+nmap -Pn -A $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
 echo "---------- Standard service detection ----------" | tee -a $PORTSCAN_RESULT
-nmap -Pn -sV $IP >> $PORTSCAN_RESULT
+nmap -Pn -sV $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
 echo "---------- Scan using TCP connect ----------" | tee -a $PORTSCAN_RESULT
-nmap -Pn -sT $IP >> $PORTSCAN_RESULT
+nmap -Pn -sT $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
 echo "---------- Scan using TCP SYN scan (default) ---------- " | tee -a $PORTSCAN_RESULT
-sudo nmap -Pn -sS $IP >> $PORTSCAN_RESULT
+sudo nmap -Pn -sS $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
 echo "---------- Scan UDP ports ---------- " | tee -a $PORTSCAN_RESULT
-sudo nmap -Pn -sU -p 123,161,162 $IP >> $PORTSCAN_RESULT
+sudo nmap -Pn -sU -p 123,161,162 $TARGET >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 echo >> $PORTSCAN_RESULT
 
